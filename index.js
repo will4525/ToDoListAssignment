6c2503e5153d81d77de272bc3645407353f5a64d
Will <126838201+will4525@users.noreply.github.com>
2025-06-05 14:45:28 -0400

    prevent all tasks from loading

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-$(".demo.index").ready(function () {
-    var getAndDisplayAllTasks = function () {
-        $.ajax({
-            type: 'GET',
-            url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=2',
-            dataType: 'json',
-            success: function (response, textStatus) {
-                $('#todo-list').empty();
-                response.tasks.forEach(function (task) {
-                    $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
-                });
-            },
-            error: function (request, textStatus, errorMessage) {
-                console.log(errorMessage);
-            }
-        });
-    }
+$(document).ready(function () {
+    const apiKey = '2';
+    const baseUrl = 'https://fewd-todolist-api.onrender.com/tasks';
 
-    var createTask = function () {
+    const createTask = function () {
         $.ajax({
             type: 'POST',
-            url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=2',
+            url: `${baseUrl}?api_key=${apiKey}`,
             contentType: 'application/json',
             dataType: 'json',
             data: JSON.stringify({
@@ -27,92 +13,79 @@ $(".demo.index").ready(function () {
                     content: $('#new-task-content').val()
                 }
             }),
-            success: function (response, textStatus) {
+            success: function (response) {
                 $('#new-task-content').val('');
-                getAndDisplayAllTasks();
+                const task = response.task;
+                $('#todo-list').append(`
+          <div class="row">
+            <p class="col-xs-8">${task.content}</p>
+            <button class="delete btn btn-danger btn-xs" data-id="${task.id}">Delete</button>
+            <label>
+              <input type="checkbox" class="mark-complete" data-id="${task.id}">
+              Complete
+            </label>
+          </div>
+        `);
             },
-            error: function (request, textStatus, errorMessage) {
-                console.log(errorMessage);
+            error: function (_, __, errorMessage) {
+                console.log('POST error:', errorMessage);
             }
         });
-    }
+    };
+
+    const deleteTask = function (id) {
+        $.ajax({
+            type: 'DELETE',
+            url: `${baseUrl}/${id}?api_key=${apiKey}`,
+            success: function () {
+                // Optionally remove from DOM
+                $(`[data-id='${id}']`).closest('.row').remove();
+            },
+            error: function (_, __, errorMessage) {
+                console.log('DELETE error:', errorMessage);
+            }
+        });
+    };
+
+    const markTaskComplete = function (id) {
+        $.ajax({
+            type: 'PUT',
+            url: `${baseUrl}/${id}/mark_complete?api_key=${apiKey}`,
+            dataType: 'json',
+            error: function (_, __, errorMessage) {
+                console.log('Complete error:', errorMessage);
+            }
+        });
+    };
+
+    const markTaskActive = function (id) {
+        $.ajax({
+            type: 'PUT',
+            url: `${baseUrl}/${id}/mark_active?api_key=${apiKey}`,
+            dataType: 'json',
+            error: function (_, __, errorMessage) {
+                console.log('Active error:', errorMessage);
+            }
+        });
+    };
 
     $('#create-task').on('submit', function (e) {
         e.preventDefault();
         createTask();
     });
 
-    getAndDisplayAllTasks();
+    $(document).on('click', '.delete', function () {
+        const id = $(this).data('id');
+        deleteTask(id);
+    });
 
-});
-
-var deleteTask = function (id) {
-    $.ajax({
-        type: 'DELETE',
-        url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=2',
-        success: function (response, textStatus) {
-            getAndDisplayAllTasks();
-        },
-        error: function (request, textStatus, errorMessage) {
-            console.log(errorMessage);
+    $(document).on('change', '.mark-complete', function () {
+        const id = $(this).data('id');
+        if (this.checked) {
+            markTaskComplete(id);
+        } else {
+            markTaskActive(id);
         }
     });
-}
 
-$(document).on('click', '.delete', function () {
-    console.log($(this).data('id'))
-});
-
-$.ajax({
-    type: 'PUT',
-    url: 'https://fewd-todolist-api.onrender.com/tasks/4/mark_complete?api_key=2',
-    dataType: 'json',
-    success: function (response, textStatus) {
-        console.log(response);
-    },
-    error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-    }
-});
-
-var markTaskComplete = function (id) {
-    $.ajax({
-        type: 'PUT',
-        url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=2',
-        dataType: 'json',
-        success: function (response, textStatus) {
-            getAndDisplayAllTasks();
-        },
-        error: function (request, textStatus, errorMessage) {
-            console.log(errorMessage);
-        }
-    });
-}
-
-$(document).on('change', '.mark-complete', function () {
-    if (this.checked) {
-        markTaskComplete($(this).data('id'));
-    }
-});
-
-var markTaskActive = function (id) {
-    $.ajax({
-        type: 'PUT',
-        url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=2',
-        dataType: 'json',
-        success: function (response, textStatus) {
-            getAndDisplayAllTasks();
-        },
-        error: function (request, textStatus, errorMessage) {
-            console.log(errorMessage);
-        }
-    });
-}
-
-$(document).on('change', '.mark-complete', function () {
-    if (this.checked) {
-        markTaskComplete($(this).data('id'));
-    } else {
-        markTaskActive($(this).data('id'));
-    }
 });
