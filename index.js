97af32fd7d707de688ab6afe75df35cba1c84183
Will <126838201+will4525@users.noreply.github.com>
2025-06-05 13:51:42 -0400

    form added

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-$.ajax({
-    type: 'POST',
-    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=2',
-    contentType: 'application/json',
-    dataType: 'json',
-    data: JSON.stringify({
-        task: {
-            content: 'Do something fun'
-        }
-    }),
-    success: function (response, textStatus) {
-        console.log(response);
-    },
-    error: function (request, textStatus, errorMessage) {
-        console.log(errorMessage);
-    }
+$(document).ready(function () {
+    $('#create-task').on('submit', function (e) {
+        e.preventDefault();
+        createTask();
+    });
 });
+
+var createTask = function () {
+    $.ajax({
+        type: 'POST',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=2',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+            task: {
+                content: $('#new-task-content').val()
+            }
+        }),
+        success: function (response, textStatus) {
+            console.log('Success:', response);
+        },
+        error: function (request, textStatus, errorMessage) {
+            console.error('Error:', errorMessage);
+        }
+    });
+};
